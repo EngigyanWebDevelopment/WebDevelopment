0ee1464498ea3fc96e5b081cf34375dd9185d524
aakash1999 <aakash.nanda99@gmail.com>
2018-07-24 21:52:23 +0530

    new commit

--- includes/cs/loginOrRegister.php
+++ includes/cs/loginOrRegister.php
@@ -1,5 +1,8 @@
 <html>
-</head><title>Please login or register</title></head>
+</head><title>Please login or register</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
 
 <body>
 
@@ -8,6 +11,39 @@
 		And if you have aleardy paid then just login and access the course
 	
 	</h1>
+	
+	
+	
+	<header class="w3-container w3-teal w3-center">
+  <h1>Register Below and click on paynow to pay for the course</h1>
+</header>
+
+<div class="w3-container  w3-margin-top">
+
+<form class="w3-container w3-card-4" method = "post" action = "https://localhost/engigyan/myRegister.php">
+<p>
+<input class="w3-input" type="text" style="width:100%" required name = "username">
+<label>Username</label></p>
+<p>
+<input class="w3-input" type="text" style="width:100%" required name = "myemail">
+<label>Email</label></p>
+<p>
+<input class="w3-input" type="password" style="width:100%" required name = "pwd1">
+<label>Password</label></p>
+
+<input class="w3-input" type="password" style="width:100%" required name = "pwd2">
+<label>Password</label></p>
+
+
+
+<p>
+<button type = "submit" class="w3-button w3-section w3-teal w3-ripple"> Pay Now </button></p>
+
+</form>
+
+</div>
+
+	
 
 
 </body>
